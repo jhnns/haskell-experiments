75a1b6bf022a76733182f63acbf50e21609ddbe8
Johannes Ewald <johannes.ewald@peerigon.com>
2016-02-13 00:23:50 +0100

    Add current state

--- Test/PointsSpec.hs
+++ Test/PointsSpec.hs
@@ -14,64 +14,50 @@ p11' = (-1, -1)
 p20 = (2, 0)
 p02 = (0, 2)
 
-ps1 = [
-    (-2, 2),   (-1, 2),   (0, 2),   (1, 2),   (2, 2),
-    (-2, 1),   (-1, 1),   (0, 1),   (1, 1),   (2, 1),
-    (-2, 0),   (-1, 0),   (0, 0),   (1, 0),   (2, 0),
-    (-2, -1),  (-1, -1),  (0, -1),  (1, -1),  (2, -1)
-    ]
-
-ps1Expected = [
-    (-2, 2),   (-1, 2),   (0, 2),   (1, 2),   (2, 2),
-    (-2, 1),                                  (2, 1),
-    (-2, 0),                                  (2, 0),
-    (-2, -1),  (-1, -1),  (0, -1),  (1, -1),  (2, -1)
-    ]
-
 main :: IO ()
 main = print $ foldl1 (++) [
 
-    top [p00] `shouldEql` p00,
-    top [p00, p01] `shouldEql` p01,
-    top [p00, p10] `shouldEql` p10,
-    top [p00, p01'] `shouldEql` p00,
-    top [p00, p00] `shouldEql` p00,
-    top [p11', p00, p11] `shouldEql` p11,
-
-    right [p00] `shouldEql` p00,
-    right [p00, p01] `shouldEql` p01,
-    right [p00, p10] `shouldEql` p10,
-    right [p00, p10'] `shouldEql` p00,
-    right [p00, p00] `shouldEql` p00,
-    right [p10', p00, p10] `shouldEql` p10,
-
-    bottom [p00] `shouldEql` p00,
-    bottom [p00, p01] `shouldEql` p00,
-    bottom [p00, p10] `shouldEql` p10,
-    bottom [p00, p01'] `shouldEql` p01',
-    bottom [p00, p00] `shouldEql` p00,
-    bottom [p11', p00, p11] `shouldEql` p11',
-
-    left [p00] `shouldEql` p00,
-    left [p00, p01] `shouldEql` p01,
-    left [p00, p10] `shouldEql` p00,
-    left [p00, p10'] `shouldEql` p10',
-    left [p00, p00] `shouldEql` p00,
-    left [p10', p00, p10] `shouldEql` p10',
-
-    center [p00] `shouldEql` p00,
-    center [p10', p10] `shouldEql` p00,
-    center [p01', p01] `shouldEql` p00,
-    center [p01', p01, p10] `shouldEql` (0.5, 0),
-    center [
-        (-1, 1),    p11,
-        p11',       (1, -1)
-    ] `shouldEql` p00,
-    center [
-        (-1, 1),    p11,
-                                p20,
-        p11',       (1, -1)
-    ] `shouldEql` (0.5, 0),
+    -- top [p00] `shouldEql` p00,
+    -- top [p00, p01] `shouldEql` p01,
+    -- top [p00, p10] `shouldEql` p10,
+    -- top [p00, p01'] `shouldEql` p00,
+    -- top [p00, p00] `shouldEql` p00,
+    -- top [p11', p00, p11] `shouldEql` p11,
+    --
+    -- right [p00] `shouldEql` p00,
+    -- right [p00, p01] `shouldEql` p01,
+    -- right [p00, p10] `shouldEql` p10,
+    -- right [p00, p10'] `shouldEql` p00,
+    -- right [p00, p00] `shouldEql` p00,
+    -- right [p10', p00, p10] `shouldEql` p10,
+    --
+    -- bottom [p00] `shouldEql` p00,
+    -- bottom [p00, p01] `shouldEql` p00,
+    -- bottom [p00, p10] `shouldEql` p10,
+    -- bottom [p00, p01'] `shouldEql` p01',
+    -- bottom [p00, p00] `shouldEql` p00,
+    -- bottom [p11', p00, p11] `shouldEql` p11',
+    --
+    -- left [p00] `shouldEql` p00,
+    -- left [p00, p01] `shouldEql` p01,
+    -- left [p00, p10] `shouldEql` p00,
+    -- left [p00, p10'] `shouldEql` p10',
+    -- left [p00, p00] `shouldEql` p00,
+    -- left [p10', p00, p10] `shouldEql` p10',
+    --
+    -- center [p00] `shouldEql` p00,
+    -- center [p10', p10] `shouldEql` p00,
+    -- center [p01', p01] `shouldEql` p00,
+    -- center [p01', p01, p10] `shouldEql` (0.5, 0),
+    -- center [
+    --     (-1, 1),    p11,
+    --     p11',       (1, -1)
+    -- ] `shouldEql` p00,
+    -- center [
+    --     (-1, 1),    p11,
+    --                             p20,
+    --     p11',       (1, -1)
+    -- ] `shouldEql` (0.5, 0),
 
     -- convexHull [] `shouldEql` [],
     -- convexHull [p00] `shouldEql` [],
@@ -86,23 +72,23 @@ main = print $ foldl1 (++) [
     -- convexHull [p00, p01, p10] `shouldEql` [p00, p01, p10],
     -- convexHull [p01, p10, p00] `shouldEql` [p01, p10, p00],
     -- convexHull [p10, p00, p01] `shouldEql` [p10, p00, p01],
-
+    --
     -- convexHull [
     --     (-1, 1),    p11,
     --     p11',       (1, -1)
-    -- ] `shouldEql` [(-1, 1), p11, (1, -1), p11'],
+    -- ] `shouldEql` [p11, (1, -1), p11', (-1, 1)],
     --
     -- convexHull [
     --     (-1, 1),    p11,
     --                             p20,
     --     p11',       (1, -1)
-    -- ] `shouldEql` [(-1, 1), p11, p20, (1, -1), p11'],
+    -- ] `shouldEql` [p11, p20, (1, -1), p11', (-1, 1)],
 
     convexHull [
         (-1, 1),    p01,        p11,
         (-0.5, 0),  p00,        (1.5, 0),
         p11',       p01'
-    ] `shouldEql` [(-1, 1), p01, p11, (1.5, 0), p01', p11'],
+    ] `shouldEql` [p11, (1.5, 0), p01', p11', (-1, 1)],
 
 
     -- convexHull [p01', p00, p10] `shouldEql` [p01', p00, p10]
--- src/Points.hs
+++ src/Points.hs
@@ -1,6 +1,7 @@
 module Points where
 
-import Data.List (foldl')
+import Data.List (foldl', elemIndex)
+import Data.Maybe (fromJust)
 import Point (Point)
 import Sort (sort)
 import Vector (angleBetween, diff)
@@ -12,27 +13,27 @@ import Debug.Trace (trace)
 convexHull :: [Point] -> [Point]
 convexHull ps
     | length ps < 3 = []
-    | length ps == 3 =
-        let
-            angle = angleBetween (head ps) (ps!!1) (ps!!2);
-            isTriangle = angle /= 0 && angle /= 180;
-            in if isTriangle then ps else []
-    | otherwise =
-        let
-            l = length ps;
-            c = center ps;
-            shift ps@(p:_) = drop 1 (ps ++ [p]);
-            rotate n xs = zipWith const (drop n (cycle xs)) xs;
-            sorted0 = sort ps c;
-            sortedR = rotate 1 sorted0;
-            sortedL = rotate (l - 1) sorted0;
-            -- triples = trace (show sortedL ++ "\n" ++ show sorted0 ++ "\n" ++ show sortedR) (zip3 sortedL sorted0 sortedR);
-            triples = zip3 sortedL sorted0 sortedR;
-            reducer ps (pl, p, pr) =
-                let
-                    angle = angleBetween p pl pr;
-                    in if trace (show pl ++ show p ++ show pr ++ show angle) angle > 180 then p:ps else ps;
-            in foldl' reducer [] triples
+    | length ps == 3 = let
+        angle = angleBetween (head ps) (ps!!1) (ps!!2);
+        isTriangle = angle /= 0 && angle /= 180;
+        in if isTriangle then ps else []
+    | otherwise = let
+        l = length ps;
+        c = center ps;
+        t = top ps;
+        rotate n xs = zipWith const (drop n (cycle xs)) xs;
+        sorted = let
+            sorted = sort ps c;
+            index = fromJust (elemIndex t sorted);
+            in rotate index sorted;
+        walk ps p1 p2 [] = ps
+        walk ps p1 p2 ps' = let
+            p3 = head ps';
+            angle = angleBetween p2 p1 p3;
+            save = walk (ps ++ [p2]) p2 p3 (tail ps');
+            skip = walk ps p1 p3 (tail ps');
+            in if angle > 180 then save else skip;
+        in walk [] t (sorted!!1) (sorted ++ [t, (sorted!!1)])
 
 center :: [Point] -> Point
 center ps

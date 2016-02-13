b3be65c4be664a2a79caf0d49486f78ce273c9b0
Johannes Ewald <johannes.ewald@peerigon.com>
2016-02-13 15:50:32 +0100

    Finish documentation

--- src/Points.hs
+++ src/Points.hs
@@ -29,15 +29,15 @@ import Debug.Trace (trace)
     greatest y coordinate. Returns an empty list if an empty list is given or
     all points are located on a line.
 -}
-{-
-    About the algorithm of convexHull
--}
 convexHull :: [Point] -> [Point]
 convexHull ps
     | length ps < 3 = []
     | otherwise = let
         -- 1. Find the starting point p1 that is guaranteed on the convex hull.
-        p1 = let
+        -- (There is one edge case: If we only have three points and all points
+        -- are located on a line, there is no convex hull and thus `start` is
+        -- NOT on the convex hull.)
+        maxY = let
             maxY p1@(_, y1) p2@(_, y2) = if y1 > y2 then p1 else p2;
             in foldl1 maxY ps;
         circle1 = let
@@ -46,29 +46,33 @@ convexHull ps
             -- 3. In order to walk all points, we use a cycled list which is
             -- rotated until the first element after the starting point is
             -- the head.
-            index = fromJust $ elemIndex p1 sorted;
+            index = fromJust $ elemIndex maxY sorted;
             in drop (index + 1) (cycle sorted)
         -- 4. Now walk around the circle and save all points to our `hull` list
         -- that have an outer angle greater than 180. We use an iteration
         -- counter i in order to track whether we have walked around the whole
         -- circle. In that case we just return `hull` as result.
         walk _ 0 hull _ _ = hull;
-        walk circle i hull p1 p2 = let
-            p3 = head circle;
-            phi = angle (diff p1 p2) (diff p3 p2);
+        walk circle i hull pl p = let
+            -- Measure the outer angle at p between pl(eft) and pr(ight).
+            pr = head circle;
+            phi = angle (diff pl p) (diff pr p);
+            -- Prepare the next step
             next = walk (tail circle) (i - 1);
-            save = next (p2:hull) p2 p3;
-            skip = next hull p1 p3;
+            -- Saves p into hull and proceeds with p as pl and pr as new p
+            save = next (p:hull) p pr;
+            -- Discards p, keeps pl and proceeds with pr as new p
+            skip = next hull pl pr;
             in if phi > 180 then save else skip;
         in walk
             (drop 1 circle1)    -- circle
             (length ps)         -- iteration counter
             []                  -- hull
-            p1
-            (head circle1)      -- p2
+            maxY                -- pl
+            (head circle1)      -- p
 
 {-|
-    Returns the center of the given point cloud. The center defined by the
+    Returns the center of the given point cloud. The center is defined by the
     intersection of the both diagonals of the smallest rectangle that enfolds
     all points.
 -}

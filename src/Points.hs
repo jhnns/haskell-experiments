434e708142984c8c47863891173b2f6a526b7fd0
Johannes Ewald <johannes.ewald@peerigon.com>
2016-02-13 04:17:38 +0100

    Refactor

--- src/Points.hs
+++ src/Points.hs
@@ -11,24 +11,22 @@ convexHull :: [Point] -> [Point]
 convexHull ps
     | length ps < 3 = []
     | otherwise = let
-        l = length ps;
-        c = center ps;
         maxY p1@(_, y1) p2@(_, y2) = if y1 > y2 then p1 else p2;
         start = foldl1 maxY ps;
         circle1 = let
-            sorted = sort ps c;
+            sorted = sort ps (center ps);
             index = fromJust $ elemIndex start sorted;
             in drop (index + 1) (cycle sorted);
         circle2 = drop 1 circle1;
-        walk 0 _ ps _ _ = ps;
-        walk i circle hull p1 p2 = let
+        walk _ 0 hull _ _ = hull;
+        walk circle i hull p1 p2 = let
             p3 = head circle;
             phi = angle (diff p1 p2) (diff p3 p2);
-            next = walk (i - 1) (tail circle);
+            next = walk (tail circle) (i - 1);
             save = next (p2:hull) p2 p3;
             skip = next hull p1 p3;
             in if phi > 180 then save else skip;
-        in walk l circle2 [] start (head circle1)
+        in walk circle2 (length ps) [] start (head circle1)
 
 center :: [Point] -> Point
 center [] = error "cannot find center of an empty list"
